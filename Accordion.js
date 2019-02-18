ecac992ed4602fcabfc4df7c58c117fe88abb184
axel-polin <univ_apolin@protonmail.com>
2019-02-18 19:44:02 +0100

    18/02/2019 POLIN Axel N°2 Modified
    Début du moteur !

--- Accordion.js
+++
@@ -1,7 +0,0 @@
-function AccFunc($scope) {
-  if (elem.className.indexOf("w3-show") == -1) {
-    elem.className += "w3-show";
-  } else { 
-    elem.className = elem.className.replace("w3-show", "");
-  }
-}
