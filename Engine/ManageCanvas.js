db4ee1884144ba598e4acc8f15bb6beaffb01347
quentin <q.piet96@gmail.com>
2019-03-02 13:53:39 +0100

    02/03/2019 Piet Quentin N°7

--- Engine/ManageCanvas.js
+++ Engine/ManageCanvas.js
@@ -1,12 +1,12 @@
 var PapyrusPict;
 
 function startCanvas() {
-  
+
   var elem =  document.getElementById("CanvasHolder");
 
   var left = elem.getBoundingClientRect().left;
   var top = elem.getBoundingClientRect().top;
-  
+
   Area.start();
   PapyrusPict = new component("1319_r_CL.JPG");
 }
@@ -14,57 +14,84 @@ function startCanvas() {
 var Area = {
   canvas : document.createElement("canvas"),
   start : function() {
-	
+
     var elem =  document.getElementById("CanvasHolder");
     var mouseIsDown = false;
-    
+
     var width = elem.getBoundingClientRect().width;
     var height = elem.getBoundingClientRect().height;
     var top = elem.getBoundingClientRect().top;
     var left = elem.getBoundingClientRect().left;
-    
+
     this.canvas.width = width;
     this.canvas.height = height;
-    this.canvas.style.cursor = "crosshair"; 
+    this.canvas.style.cursor = "crosshair";
     this.context = this.canvas.getContext("2d");
-      
+
     elem.appendChild(this.canvas);
-    
+
     this.interval = setInterval(updateArea, 30);
-    
+
     window.addEventListener('mousemove', function (e) {
       if (!mouseIsDown) return ;
       Area.x = e.clientX - left;
       Area.y = e.clientY - top;
-      
-      if (Area.x <= 0){
+
+      if (Area.x /*- PapyrusPict.image.width/2*/<= 0){
         mouseIsDown = false;
-        Area.x = PapyrusPict.image.width/2;
+        Area.x = PapyrusPict.image.width/2 + 1;
+        //mouseIsDown = true;
+
       }
-      if (Area.y <= 0){
+      if (Area.x /*+ PapyrusPict.image.width/2*/>= width) {
         mouseIsDown = false;
-        Area.y = PapyrusPict.image.height/2;
+        Area.x = width - PapyrusPict.image.width/2 - 1; //si fenêtre réduite
+        //mouseIsDown = true;
+
+
       }
-      if (Area.y >= height){
+      if (Area.y /*- PapyrusPict.image.height/2*/ <= 0){
         mouseIsDown = false;
-        Area.y = height - PapyrusPict.image.height/2;
+        Area.y = PapyrusPict.image.height/2 + 1;
+        //mouseIsDown = true;
+      }
+      if (Area.y /*+ PapyrusPict.image.height/2*/ >= height){
+        mouseIsDown = false;
+        Area.y = height - PapyrusPict.image.height/2 - 1;
+        //mouseIsDown = true;
+
 }
     });
-    
+
     window.addEventListener('mousedown', function (e) {
+    /*  var posX = e.clientX - left;
+      var posY = e.clientY - top;
+      var PapyrusLeft = PapyrusPict.x-PapyrusPict.image.width/2;
+      var PapyrusRight = PapyrusPict.x+PapyrusPict.image.width/2;
+      var PapyrusTop = PapyrusPict.y - PapyrusPict.image.height/2;
+      var PapyrusBottom = PapyrusPict.y + PapyrusPict.image.height/2;
+        //test si curseur est positionné sur l'image
+      if (posX >= PapyrusLeft  && posX <= PapyrusRight  && posY >= PapyrusTop && posY <= PapyrusBottom){
+        mouseIsDown = true;
+      }
+      else {
+        mouseIsDown = false;
+      }
+      */
       mouseIsDown = true;
+
     });
-    
-    window.addEventListener('mouseup', function () {
+
+    window.addEventListener('mouseup', function (e) {
       mouseIsDown = false;
     });
-    
+
     window.addEventListener('keydown', function (e) {
       e.preventDefault();
       Area.keys = (Area.keys || []);
       Area.keys[e.keyCode] = (e.type == "keydown");
     });
-    
+
     window.addEventListener('keyup', function (e) {
       this.angle = 0;
       Area.keys[e.keyCode] = (e.type == "keydown");
@@ -80,11 +107,11 @@ var Area = {
 
 
 function component(src) {
-   
+
     this.image = new Image();
-    
+
     this.image.onload = function() {
-  
+
       if (this.naturalWidth > this.naturalHeight) {
         while (this.width > (Area.canvas.width/1.5)) {this.width = this.width*0.9;this.height = this.height*0.9};
       }
@@ -93,13 +120,13 @@ function component(src) {
       }
     }
     this.image.src = src;
-    
+
     this.x = Area.canvas.width/2;
     this.y = Area.canvas.height/2;
-   
+
     this.angle = 0;
     this.scale = 1;
-    
+
     this.update = function() {
         ctx = Area.context;
         ctx.save();
@@ -115,7 +142,7 @@ function updateArea() {
   Area.clear();
   var scale = 1;
   var ctx = Area.context;
-  
+
   if (Area.x && Area.y) {
     PapyrusPict.x = Area.x;
     PapyrusPict.y = Area.y;
@@ -123,21 +150,22 @@ function updateArea() {
 
   if (Area.keys && (Area.keys[37] || Area.keys[81])) {PapyrusPict.angle -= 5 * Math.PI / 180}
   if (Area.keys && (Area.keys[39] || Area.keys[68])) {PapyrusPict.angle += 5 * Math.PI / 180}
-   
+
   if (Area.keys && Area.keys[73]) {
     if (PapyrusPict.scale < 1.5) {
       PapyrusPict.scale += 0.05;
+
     };
   };
-  
+
   if (Area.keys && Area.keys[79]) {
     if (PapyrusPict.scale > 0.2) {
       PapyrusPict.scale -= 0.05;
     };
   };
-  
-  if (Area.keys && Area.keys[77]) {displayMeta();}; 
-  
+
+  if (Area.keys && Area.keys[77]) {displayMeta();};
+
   if (Area.keys && Area.keys[116]) {location.reload(true);};
   PapyrusPict.update();
 }
