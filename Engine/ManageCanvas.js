b4eb042a6f8ba86cba83e114d4f6fb55bd79cd33
quentin <q.piet96@gmail.com>
2019-03-12 10:32:45 +0100

    correctif sroll sidebar et sortie canvas

--- Engine/ManageCanvas.js
+++ Engine/ManageCanvas.js
@@ -6,9 +6,10 @@ function startCanvas() {
 
   var left = elem.getBoundingClientRect().left;
   var top = elem.getBoundingClientRect().top;
-  
+
   Area.start();
   //addComponent(Area);
+
 }
 
 var Area = {
@@ -17,44 +18,71 @@ var Area = {
 
     var elem =  document.getElementById("CanvasHolder");
     var mouseIsDown = false;
-    
+
     var width = elem.getBoundingClientRect().width;
     var height = elem.getBoundingClientRect().height;
     var top = elem.getBoundingClientRect().top;
     var left = elem.getBoundingClientRect().left;
     this.expLeft = left;
     this.expTop = top;
+    this.mode = "move";
 
     this.canvas.width = width;
     this.canvas.height = height;
     this.canvas.style.cursor = "crosshair";
     this.context = this.canvas.getContext("2d");
     this.context.globalCompositeOperation='lighter';
-    
+
     this.images = [];
     this.selection = null;
 
+
     elem.appendChild(this.canvas);
 
+
+
     this.interval = setInterval(updateArea, 30);
-      
+
     window.addEventListener('mousemove', function (e) {
+
+      var cw = Area.canvas.width;
+      var ch = Area.canvas.height;
+
       if (!mouseIsDown) return ;
+
       Area.x = e.clientX - left;
       Area.y = e.clientY - top;
       Area.selection.x = Area.x;
       Area.selection.y = Area.y;
+
+      if (Area.x < 0){
+        mouseIsDown = false;
+        Area.selection.x = 0;
+      }
+      if (Area.x > cw){
+        mouseIsDown  = false;
+        Area.selection.x = cw;
+      }
+      if (Area.y < 0) {
+        mouseIsDown = false;
+        Area.selection.y = 0;
+      }
+      if (Area.y > ch) {
+        mouseIsDown = false;
+        Area.selection.y = ch;
+      }
+      mouseIsDown = true;
     });
 
     window.addEventListener('mousedown', function (e) {
       if (e.button == 0){
-        
+
         var mx = e.clientX;
         var my = e.clientY;
 
         var images = Area.images;
         var l = images.length;
-        
+
         for (var i = l-1; i >= 0; i--) {
           if (images[i].contains(mx,my)) {
             var mySel = images[i];
@@ -76,7 +104,7 @@ var Area = {
     window.addEventListener('keydown', function (e) {
       e.preventDefault();
       Area.keys = (Area.keys || []);
-      
+
       if (e.keyCode == 27) {
         if (!stop){
           Area.stop();
@@ -87,53 +115,54 @@ var Area = {
           stop = false;
         }
       };
-      
+
       if (e.keyCode == 77) {displayMeta()};
-      
+
       if (e.keyCode == 116) {location.reload(true)};
-      
+
       if(Area.selection != null) {
-        
+
         Area.keys[e.keyCode] = (e.type == "keydown");
-        
+
         if (Area.keys && (Area.keys[37] || Area.keys[81])) {Area.selection.angle -= 5 * Math.PI / 180};
-        
+
         if (Area.keys && (Area.keys[39] || Area.keys[68])) {Area.selection.angle += 5 * Math.PI / 180};
-        
+
         if (Area.keys && Area.keys[73] && Area.selection.scale < 1.5) {Area.selection.scale += 0.02};
-      
+
         if (Area.keys && Area.keys[79] && Area.selection.scale > 0.2) {Area.selection.scale -= 0.02};
-        
+
         if (Area.keys && Area.keys[82]) {Area.selection.remove()};
-        
+
       }
-      
+
     });
 
     window.addEventListener('keyup', function (e) {
       this.angle = 0;
       Area.keys[e.keyCode] = (e.type == "keydown");
     });
-    
+
     window.addEventListener("wheel", function(e) {
+      if (e.clientX < left) return ;
       if(Area.selection != null) {
         if (e.deltaY > 0 && Area.selection.scale > 0.2) {Area.selection.scale -= 0.05};
         if (e.deltaY < 0 && Area.selection.scale < 1.5) {Area.selection.scale += 0.05};
       }
     });
-    
+
     window.addEventListener("resize", function() {
       var elem =  document.getElementById("CanvasHolder");
       var ctx = Area.context;
       var images = Area.images;
       var l = images.length;
-      
+
       var width = elem.getBoundingClientRect().width;
       var height = elem.getBoundingClientRect().height;
-      
+
       ctx.canvas.width = width;
       ctx.canvas.height = height;
-      
+
     });
   },
   clear : function() {
@@ -148,7 +177,7 @@ function component(src,ref) {
 
     this.image = new Image();
     this.image.onload = function() {
-    
+
       if (this.naturalWidth > this.naturalHeight) {
         while (this.width > (Area.canvas.width/1.5)) {this.width = this.width*0.9;this.height = this.height*0.9};
       }
@@ -158,15 +187,15 @@ function component(src,ref) {
     }
     this.image.src = src;
     this.ref = ref;
-    
+
     this.x = Area.canvas.width/2;
     this.y = Area.canvas.height/2;
 
     this.angle = 0;
     this.scale = 1;
-    
+
     this.setOpp = true;
-    
+
     this.update = function() {
         var ctx = Area.context;
         ctx.save();
@@ -177,7 +206,7 @@ function component(src,ref) {
         ctx.drawImage(this.image,this.image.width / -2, this.image.height / -2, this.image.width, this.image.height);
         ctx.restore();
     }
-    
+
 }
 
 component.prototype.contains = function(mx, my) {
